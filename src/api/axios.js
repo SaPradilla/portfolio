6df6e053d6bea52b2c55b500a2388aab6147fd38
SaPradi <a.garces@globant.com>
2024-10-28 12:12:46 -0500

    feat: axios api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    // baseURL: 'https://portfolio-proyects-api.vercel.app/api/'
-    baseURL: 'http://localhost:3000/api/'
+    baseURL: 'https://portfolio-proyects-api.vercel.app/api/'
+    // baseURL: 'http://localhost:3000/api/'
 })
 
 export default api
